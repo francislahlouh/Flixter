67cc8ff0eaa31f69465a0da95c6f0dd70a3daa71
Francis Lahlouh <francislahlouh123@yahoo.com>
2020-01-05 02:54:06 -0800

    added newer values

--- Gemfile
+++ Gemfile
@@ -69,3 +69,4 @@ gem 'simple_form'
 gem 'devise'
 gem 'carrierwave', '~> 2.0'
 gem "mini_magick"
+gem 'carrierwave-aws'
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  storage :fog
+  storage :aws
 
   # A bunch of comments
 
