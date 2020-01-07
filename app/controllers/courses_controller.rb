ec9667147e138724c9aa8e4d24473c6ca8d25aca
Francis Lahlouh <francislahlouh123@yahoo.com>
2020-01-06 16:32:52 -0800

    new

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,4 @@
-::CoursesController < ApplicationController
+:CoursesController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:show]
 
