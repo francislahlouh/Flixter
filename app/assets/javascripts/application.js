d602a8ee3ccbdbc9feb7d615b50b578c058d2b1d
Francis Lahlouh <francislahlouh123@yahoo.com>
2020-01-14 16:36:58 -0800

    added new change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-uj
 //= require activestorage
 //= require jquery
 //= require jquery_ujs
